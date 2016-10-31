74a40556b2f4f5e0c91ac4469ef25367284b2d7f
Noam <noam.ascher@gmail.com>
2016-10-31 22:33:32 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this line makes it so that when 'hereClicked' is invoked
+    // as an event handler, 'this' inside of the function will
+    // refer to the current App object.
+    this.hereClicked = this.hereClicked.bind(this);
+    this.state = {
+      meters: 42
+    };
+  }
+  hereClicked() {
+    this.setState({
+      meters: 113
+    });
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <form>
+          <input name="meters" />
+          <button>Convert</button>
+        </form>
+        <button onClick={this.hereClicked}>Click Here!</button>
+        <div>{this.state.meters}</div>
+      </div>
     );
   }
 }
+
 export default App;
